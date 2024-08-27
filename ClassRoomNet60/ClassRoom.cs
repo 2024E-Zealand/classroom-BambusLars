d09046492c7ee97feb583c45269cacf0aad034dc
Sebastian Karlsson <sebastianwyrwik16@gmail.com>
2024-08-27 14:04:48 +0200

    Task 2 done

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,18 @@
+using System.Reflection.Emit;
+
+namespace ClassRoomNet60;
+
+public class ClassRoom
+{
+ public string Name { get; set; }
+ public int BrithMonth { get; set; }
+ public int Birthday { get; set; }
+
+
+ public ClassRoom()
+ {
+  
+ }
+ 
+}
+
